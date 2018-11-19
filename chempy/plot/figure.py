5c064a9d33ce731830d3ac6f4d981aa97d25daec
Antoine Laborde <antoine.laborde@greentropism.com>
2018-11-19 09:00:17 +0100

    figure display
    introduce the ycolor input argument

--- chempy/plot/figure.py
+++ chempy/plot/figure.py
@@ -7,27 +7,43 @@
 import chempy.utils.util as util
 import chempy.utils.classes as classes
 
+import numpy as np
 
 import matplotlib as mpl
+mpl.use('Qt5Agg')
 import matplotlib.pyplot as plt
+
 
 from collections import OrderedDict
 
+"""
+Idees pour changement de style:
+utiliser style pyplot context
 
-def curve(div, i=None, cmap='hsv', legend_label=None, legend=True): 
+"""
+
+
+def curve(div, row=None, cmap='hsv', legend_label=None, legend=True, ycolor=None): 
     """
     Plots one or several row as curves 
         
     Parameters
     ----------
     div: div or list of div(mandatory)
-
-    i: list (optional, default=None)
+        div to display
+    row: list (optional, default=None)
         name of the rows to select or indexes of row to select in a list
+    cmap: str (optional, default='hsv')
+        name of the colormap
+    legend_label: list of str (optional, None)
+        list of str for legend, must be the same number of div in div list
+    legend: bool (optional, default=True)
+        display legend or not
+    ycolor: div (optional, default=None)
+        div with values for colors (with one col), only valid for single div input
 
     Notes
     -----
-
     if div is a list of div, each div in the list is plot with a different color
     
     """
@@ -35,56 +51,82 @@ def curve(div, i=None, cmap='hsv', legend_label=None, legend=True):
     # Check if div is a list of div or a div
     if isinstance(div, list):
         for ind, div_ in enumerate(div):
-            if isinstance(div_, classes.Div):
+            if not(isinstance(div_, classes.Div)):
                 raise ValueError('Element ' + str(ind) + ' of your list is not a Div')
         curve_type = 'list'
     elif not(isinstance(div, classes.Div)):
         raise ValueError('div must be a div instance or a list of div instances')
     else:
         curve_type = 'one'
-    
     # Select rows of div
-    if i is not None:
+    if row is not None:
         if isinstance(div, list):
-            print('i index is ignored when div is a list')
+            print('row index is ignored when div is a list')
         else:
-            div = util.selectrow(div, i)
+            div = util.selectrow(div, row)
+    # Check and extract ycolor
+    if ycolor is not None:
+        if not(isinstance(ycolor, classes.Div)):
+            raise ValueError('ycolor must be a div')
+        if ycolor.d.shape[1] != 1:
+            raise ValueError('ycolor must be a div with 1 col (not ' + str(div.d.shape[1]) + ')')
+        if curve_type == 'list':
+            raise ValueError('ycolor option is not valid for a list of div input')
+        if ycolor.d.shape[0] != div.d.shape[0]:
+            raise ValueError('ycolor must have the same number of rows than div')
+        ycolormap, scalarmap = get_cmap(ycolor.d, cmap)
+    else:
+        ycolormap = None
 
-
-    # Check if div.v can be interpreted as numericals
-    x_vector = util.vfield2num(div)
-    if x_vector is None:
-        x_vector = np.arange(div.v.shape[0])
-
-    # one div case
+    # One div case
     if curve_type == 'one':
+        # Check if div.v can be interpreted as numericals
+        x_vector = util.vfield2num(div)
+        if x_vector is None:
+            x_vector = np.arange(div.v.shape[0])
+
         fig = plt.figure()
         ax = fig.add_subplot(111)
-        ax.plot(x_vector, div.d.T)
+        
+        if ycolormap is None:
+            ax.plot(x_vector, div.d.T)
+        else:
+            for i in range(div.d.shape[0]):
+                ax.plot(x_vector, div.d[i,:].T, c=ycolormap[ycolor.d[i,0]])
+            fig.colorbar(scalarmap, label = ycolor.v[0])
         plt.show()
+    
     # list of div case
     elif curve_type == 'list':
+        # Check legend label has to right number of element
+        if legend_label is not None:
+            if len(legend_label) != len(div):
+                print('Warning! legend_label contains ' + str(len(legend_label) + ' elements but should contain ' + str(len(div))))
+                legend_label = None
+        if legend_label is None:
+            legend_label = [str(row) for row in range(len(div))]
+
         cmap_vec = mpl.cm.get_cmap(cmap)
         fig = plt.figure()
         ax = fig.add_subplot(111)
         ind = 0
-        for div_group, filtername in zip(group_obj.div_list, group_obj.filter_list):
+        for div_group, label in zip(div, legend_label):
             ind += 1
-            ax.plot(x_vector, div_group.d.T,label=filtername, c=cmap_vec(ind/len(group_obj.div_list)))
+            # Check if div.v can be interpreted as numericals
+            x_vector = util.vfield2num(div_group)
+            if x_vector is None:
+                x_vector = np.arange(div.v.shape[0])
+
+            ax.plot(x_vector, div_group.d.T,label=str(label), c=cmap_vec(ind/len(div)))
         # Handle legend
-        if legend and legend_label is None:
+        if legend:
             handles, labels = ax.get_legend_handles_labels()
             by_label = OrderedDict(zip(labels, handles))
             leg = plt.legend(by_label.values(), by_label.keys(), title='Legend')
             leg.draggable()
-        if legend and legend_label is not None:
-            if len(legend_label) != ind:
-                raise ValueError('legend_label must contains ' + str(ind) + 'elements')
-
-            leg = plt.legend(legend_label, title='Legend')
-            leg.draggable()
         plt.show()
 
+#def map2(div, col_x, col_y, )
 
 def map(div,col1,col2,margin=0.1,group='',fontsize=20):
    """
@@ -121,4 +163,28 @@ def map(div,col1,col2,margin=0.1,group='',fontsize=20):
             plt.text(div.d[i,col1],div.d[i,col2],div.i[i],color=thiscolor,fontsize=20)
    plt.xlabel(div.v[col1])   
    plt.ylabel(div.v[col2])
-   plt.show() 
+   plt.show()
+
+def get_cmap(y, cmap):
+    """
+        get_cmap sends back color information according to y values
+
+        Parameters
+        ----------
+        y: array (mandatory)
+            array of values to use for color
+        cmap: str (mandatory)
+            cmap name
+        Returns
+        -------
+        colors: array
+    """
+
+    normcolor = mpl.colors.Normalize(vmin = np.nanmin(y), vmax = np.nanmax(y))
+    scalarmap = mpl.cm.ScalarMappable(norm = normcolor, cmap = cmap)
+    unique_y = np.asarray(np.unique(y[~np.isnan(y)]))
+    colors = {}
+    for yval in unique_y:
+        colors[yval] = scalarmap.to_rgba(yval)
+    scalarmap.set_array([])
+    return colors, scalarmap
